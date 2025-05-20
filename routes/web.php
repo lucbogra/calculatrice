72b7c17ac24956311b780c4854790b0f1bbcbbfa
lucbogra <53279389+lucbogra@users.noreply.github.com>
2025-05-20 15:35:49 +0100

    redirect / to calculator page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('calculator.index'));
 });
 
 Route::get('/calculator', [CalculatorController::class, 'index'])->name('calculator.index');
